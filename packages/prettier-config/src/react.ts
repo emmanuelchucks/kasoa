bc1f6003c654499628f7019ebf5799edda3327ff
Emmanuel Chucks <echucks19@gmail.com>
2025-12-17 03:26:30 +0000

    fix(prettier-config): use named export for react config

--- packages/prettier-config/src/react.ts
+++ packages/prettier-config/src/react.ts
@@ -1,6 +1,6 @@
 import type { Config } from "prettier";
 
-export default {
+export const react = {
   plugins: ["prettier-plugin-tailwindcss"],
   tailwindAttributes: ["/.*ClassName/"],
   tailwindFunctions: ["tv"],
