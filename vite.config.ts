4f6b62f3ab885f3d98ccbcfeb2553d4dbaeaa93a
Emmanuel Chucks 🇬🇭 <31349069+emmanuelchucks@users.noreply.github.com>
2026-04-14 22:16:27 +0000

    chore: move root tasks into package scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,6 @@ import { createMonorepoConfig } from "./packages/vite-plus-config/src/monorepo/i
 export default createMonorepoConfig({
   run: {
     tasks: {
-      build: {
-        command: "vp run -r build",
-      },
-      check: {
-        command: "vp run -r check",
-      },
-      "check:fix": {
-        command: "vp run -r check:fix",
-      },
-      test: {
-        command: "vp run -r test",
-      },
       release: {
         cache: false,
         command: "pnpm exec changeset publish",
